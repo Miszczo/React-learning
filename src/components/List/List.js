6e2a703716b782e262b4cd81e98b088e84aabaf7
Miszczoo <mcglowack@gmail.com>
2020-12-06 15:44:57 +0100

    Add container to List and Search

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -6,6 +6,7 @@ import Column from '../Column/ColumnContainer.js';
 import { settings } from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
 import Creator from '../Creator/Creator.js';
+import Container from '../Container/Container';
 
 class List extends React.Component {
   static propTypes = {
@@ -24,24 +25,26 @@ class List extends React.Component {
 
     return (
       <section className={styles.component}>
-        <Hero 
-          titleText={title} 
-          image={image} 
-        />
-        <div className={styles.description}>
-          {ReactHtmlParser(description)}
-        </div>
-        <div className={styles.columns}>
-          {columns.map((columnData) => (
-            <Column key={columnData.id} {...columnData} />
-          ))}
-        </div>       
-        <div className={styles.creator}>
-          <Creator 
-            text={settings.columnCreatorText} 
-            action={title => addColumn(title)}
+        <Container>
+          <Hero 
+            titleText={title} 
+            image={image} 
           />
-        </div>
+          <div className={styles.description}>
+            {ReactHtmlParser(description)}
+          </div>
+          <div className={styles.columns}>
+            {columns.map((columnData) => (
+              <Column key={columnData.id} {...columnData} />
+            ))}
+          </div>       
+          <div className={styles.creator}>
+            <Creator 
+              text={settings.columnCreatorText} 
+              action={title => addColumn(title)}
+            />
+          </div>
+        </Container>
       </section>
     );
   }
